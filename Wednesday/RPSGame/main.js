f3378e14f303cfb7c5f86ab83706a103bf4b6be4
Myron Anthony <codehearted12@gmail.com>
2017-07-05 15:59:58 -0400

    JavaIntro

---
+++ Wednesday/RPSGame/main.js
@@ -0,0 +1,20 @@
+// Variables
+const computer = 'paper';
+let me = 'scissors'; // Change this to test your code
+
+
+
+
+if (me === 'rock') {
+    console.log('I Win!');
+}
+
+else if (me === 'paper') {
+
+  console.log('Looks like a tie!');
+}
+
+
+else if (me equaps scissors){
+console.log('Looks like a tie!');
+}
---
+++ main.js
@@ -0,0 +1,28 @@
+let x = true;
+let y = false;
+let n = null;
+let q = "hello";
+let b = undefined;
+let a = 5;
+
+
+console.log(x)
+console.log(y)
+console.log(n)
+console.log(q)
+console.log(b)
+console.log(a)
+
+console.log (typeof x);
+console.log (typeof y);
+console.log (typeof n);
+console.log (typeof q);
+console.log (typeof b);
+console.log (typeof a);
+
+a = a + 5;
+console.log(a);
+
+x = x + 1;
+console.log(x);
+console.log( typeof x);
